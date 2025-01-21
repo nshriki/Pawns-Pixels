0801f89ff3916b2bc109d3f8f5e47bdd0b1ba953
nshriki <brensohon731@gmail.com>
2025-01-21 18:00:46 +0800

    V1 Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
